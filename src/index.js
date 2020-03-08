0422a805f2261cb764c7dce15d23876c3166bd93
Michael Lage <gs.urmomma@gmail.com>
2020-03-08 11:45:27 -0600

    working game
    - calculate winner function
    - status message updated to display winner or who is currently playing

--- src/index.js
+++ src/index.js
@@ -41,11 +41,20 @@ class Board extends React.Component {
     }
 
     render() {
-        const nextPlayer = this.state.xIsNext ? 'X' : 'O';
+        const winner = calculateWinner(this.state.squares);
+
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            const currentPlayer = this.state.xIsNext ? 'X' : 'O';
+            status = 'Current player: ' + currentPlayer;
+        }
+
 
         return (
             <div>
-                <div className="status">Next player: {nextPlayer}</div>
+                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -80,6 +89,26 @@ class Game extends React.Component {
             </div>
         );
     }
+}
+
+const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
 }
 
 // ========================================
