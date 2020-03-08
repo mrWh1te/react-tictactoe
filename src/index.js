fff199e9394c00f49cee554bc2b666df652f4b0d
Michael Lage <gs.urmomma@gmail.com>
2020-03-08 11:06:22 -0600

    adding state, using setState to change it

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null
+        }
+    }
+
     render() {
         return (
-        <button className="square" onClick={() => { alert('click') }}>
-            {this.props.value}
-        </button>
+            <button className="square" onClick={() => { this.setState({value: 'X'}) }}>
+                {this.state.value}
+            </button>
         );
     }
 }
@@ -21,24 +28,24 @@ class Board extends React.Component {
         const status = 'Next player: X';
 
         return (
-        <div>
-            <div className="status">{status}</div>
-            <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
             </div>
-            <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            </div>
-            <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-            </div>
-        </div>
         );
     }
 }
@@ -46,15 +53,15 @@ class Board extends React.Component {
 class Game extends React.Component {
     render() {
         return (
-        <div className="game">
-            <div className="game-board">
-            <Board />
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
             </div>
-            <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-            </div>
-        </div>
         );
     }
 }
